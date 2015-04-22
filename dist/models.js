670fee49171c13e3af3af27e2ba047afc98208e3
Colin Stannard <colin.stannard@cbsindustries.com>
2015-04-23 00:27:17 +0100

    Forgot to build -AGAIN!!!-

--- dist/models.js
+++ dist/models.js
@@ -2598,6 +2598,42 @@ TravelDuck_AvailabilityCalendar.prototype.attachMouseEventsToDatepicker = functi
     }, 0);
   }
 };
+TravelDuck_NewsletterSubscriber.prototype.constructor = TravelDuck_NewsletterSubscriber;
+
+
+/**
+ * Construct a Newsletter Subscriber.
+ *
+ * @constructor
+ * @param email
+ */
+function TravelDuck_NewsletterSubscriber(email) {
+  this.email = email
+}
+
+
+/**
+ * Subscribe the given email address to the TravelDuck newsletter.
+ *
+ * @param emailAddress
+ * @param successCallback
+ * @param failureCallback
+ */
+TravelDuck_NewsletterSubscriber.subscribeEmailAddress = function(emailAddress, successCallback, failureCallback) {
+  $.ajax({
+    url: "https://travelduck.co/api/rest/v1/newsletter/subscribe?jsoncallback=?",
+    method: "POST",
+    dataType: 'jsonp',
+    crossDomain: true,
+    cache: false,
+    timeout: 10000,
+    data: {
+      "email": emailAddress
+    },
+    success: successCallback,
+    error: failureCallback
+  });
+};
 TravelDuck_Photo.prototype.constructor = TravelDuck_Photo;
 
 
